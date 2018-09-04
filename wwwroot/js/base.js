96e9483e3f6abb305684b39c9eb8fc348bf0e18f
x1911 <bube_icq@yahoo.de>
2018-09-04 10:43:28 +0800

    时间轴bug修复

--- wwwroot/js/base.js
+++ wwwroot/js/base.js
@@ -27,11 +27,17 @@ function gotoCN() {
 /**
  * 标签页快速切换
  */
+var isHoving
 function tabFastSwitch() {
   $('.tab-fast-switch').hover(function () {
-    console.log('this.', $(this))
-    // $('.tab-fast-switch').tab('hide')
-    $( this ).tab('show')
+    var t = $( this )
+    if(isHoving) return
+    isHoving = true
+    setTimeout(function () {
+      t.tab('show')
+      isHoving = false
+    }, 300)
+    // console.log('this.', $(this))
   })
 	// $('.tab-fast-switch').on('mouseover', function() {
 	// 	$(this).tab('show');
